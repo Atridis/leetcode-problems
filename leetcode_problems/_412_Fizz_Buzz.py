f1e875dfed611500623c53b23ee5ed9c7bd21f39
Бахтиев Рамиз <bahr@1c.ru>
2020-03-03 12:44:22 +0300

    syntax fixes

--- leetcode_problems/_412_Fizz_Buzz.py
+++ leetcode_problems/_412_Fizz_Buzz.py
@@ -1,5 +1,8 @@
 from typing import List
 
 class Solution:
+    def fizzBuzzString(self, i: int):
+        return 'Fizz' if i % 3 == 0 else '' + 'Buzz' if i % 5 == 0 else '' + '' if i % 3 == 0 or i % 5 == 0 else str(i)
+
     def fizzBuzz(self, n: int) -> List[str]:
-        return [('Fizz' if i % 3 == 0 else '') + ('Buzz' if i % 5 == 0 else '') + (str(i) if i % 3 != 0 and i % 5 != 0 else '') for i in range(1, n + 1)]
+        return [self.fizzBuzzString(i) for i in range(1, n + 1)]
