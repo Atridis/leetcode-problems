f481b5f63ea4571cbf1e9b2eb4d22e77873428b7
Бахтиев Рамиз <bahr@1c.ru>
2020-03-03 14:34:08 +0300

    pytest launch fix

--- leetcode_problems/_412_Fizz_Buzz.py
+++ leetcode_problems/_412_Fizz_Buzz.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def fizzBuzzString(self, i: int):
-        return 'Fizz' if i % 3 == 0 else '' + 'Buzz' if i % 5 == 0 else '' + '' if i % 3 == 0 or i % 5 == 0 else str(i)
+        return 'Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0) + str(i) * (i % 3 != 0 and i % 5 != 0)
 
     def fizzBuzz(self, n: int) -> List[str]:
         return [self.fizzBuzzString(i) for i in range(1, n + 1)]
